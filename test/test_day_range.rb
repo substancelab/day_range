ec61308f0a1104c7499da09c9870e69f329b99e9
Jakob Skjerning <jakob@mentalized.net>
2022-09-29 12:54:58 +0200

    Extract factory method

--- test/test_day_range.rb
+++ test/test_day_range.rb
@@ -15,27 +15,22 @@ class TestDayRange < Minitest::Test
   end
 
   def test_it_responds_to_begin
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     assert_equal Date.new(2021, 1, 1), day_range.begin
   end
 
   def test_it_responds_to_end
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     assert_equal Date.new(2021, 1, 3), day_range.end
   end
 
   def test_it_responds_to_first
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     assert_equal Date.new(2021, 1, 1), day_range.first
   end
 
   def test_it_responds_to_last
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     assert_equal Date.new(2021, 1, 3), day_range.last
   end
 
   def test_it_generates_an_array_of_dates
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     dates = day_range.to_a
     assert_equal [
       Date.new(2021, 1, 1),
@@ -45,33 +40,34 @@ class TestDayRange < Minitest::Test
   end
 
   def test_days_returns_the_number_of_days_in_the_range
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     assert_equal 3, day_range.days
   end
 
   def test_previous_returns_a_day_range_with_the_same_length_as_the_original
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     previous = day_range.previous
     assert_equal 3, previous.days
   end
 
   def test_previous_returns_a_day_range_that_ends_on_the_day_before_the_originals_start
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     previous = day_range.previous
     assert_equal Date.new(2020, 12, 29), previous.first
     assert_equal Date.new(2020, 12, 31), previous.last
   end
 
   def test_next_returns_a_day_range_with_the_same_length_as_the_original
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     next_range = day_range.next
     assert_equal 3, next_range.days
   end
 
   def test_next_returns_a_day_range_that_starts_on_the_day_after_the_originals_end
-    day_range = DayRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 3))
     next_range = day_range.next
     assert_equal Date.new(2021, 1, 4), next_range.first
     assert_equal Date.new(2021, 1, 6), next_range.last
+  end
+
+  private
+
+  def day_range(first: Date.new(2021, 1, 1), last: Date.new(2021, 1, 3))
+    DayRange.new(first, last)
   end
 end
